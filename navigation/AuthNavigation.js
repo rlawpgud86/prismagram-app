e12e55329688b6d84238ada73e6f2a43e0959613
guest140 <an470@ypla.infosys>
2020-04-27 09:52:26 +0700

    problem

---
+++ navigation/AuthNavigation.js
@@ -0,0 +1,21 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignUp from "../screens/Auth/Signup";
+import Confirm from "../screens/Auth/Confirm";
+import Login from "../screens/Auth/Login";
+import AuthHome from "../screens/Auth/AuthHome";
+
+const AuthNavigation = createStackNavigator();
+
+export default () => (
+  <NavigationContainer>
+    <AuthNavigation.Navigator initialRouteName="AuthHome" headerMode="none">
+      <AuthNavigation.Screen name="HomeScreen" component={AuthHome} />
+      <AuthNavigation.Screen name="Login" component={Login} />
+      <AuthNavigation.Screen name="Signup" component={SignUp} />
+      <AuthNavigation.Screen name="Confirm" component={Confirm} />
+    </AuthNavigation.Navigator>
+  </NavigationContainer>
+);
