5d928c5c195fdc5c919dd87e67840361c1e0ffc2
guest140 <an470@ypla.infosys>
2020-05-05 08:25:35 +0700

    app

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -2,7 +2,7 @@ import "react-native-gesture-handler";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SignUp from "../screens/Auth/Signup";
+import Signup from "../screens/Auth/Signup";
 import Confirm from "../screens/Auth/Confirm";
 import Login from "../screens/Auth/Login";
 import AuthHome from "../screens/Auth/AuthHome";
@@ -11,10 +11,10 @@ const AuthNavigation = createStackNavigator();
 
 export default () => (
   <NavigationContainer>
-    <AuthNavigation.Navigator initialRouteName="AuthHome" headerMode="none">
+    <AuthNavigation.Navigator headerMode="none">
       <AuthNavigation.Screen name="AuthHome" component={AuthHome} />
       <AuthNavigation.Screen name="Login" component={Login} />
-      <AuthNavigation.Screen name="Signup" component={SignUp} />
+      <AuthNavigation.Screen name="Signup" component={Signup} />
       <AuthNavigation.Screen name="Confirm" component={Confirm} />
     </AuthNavigation.Navigator>
   </NavigationContainer>
---
+++ navigation/TabNavigation.js
@@ -0,0 +1,23 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Home from "../screens/Home";
+import Profile from "../screens/Profile";
+import Search from "../screens/Search";
+import Notifications from "../screens/Notifications";
+
+const TabNavigation = createBottomTabNavigator();
+
+export default () => {
+  return (
+    <NavigationContainer>
+      <TabNavigation.Navigator>
+        <TabNavigation.Screen name="Home" component={Home} />
+        <TabNavigation.Screen name="Profile" component={Profile} />
+        <TabNavigation.Screen name="Search" component={Search} />
+        <TabNavigation.Screen name="Notifications" component={Notifications} />
+      </TabNavigation.Navigator>
+    </NavigationContainer>
+  );
+};
