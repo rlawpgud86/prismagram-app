4114076d562b1f4eb0de34ef38417d5adba5e5a3
guest140 <an470@ypla.infosys>
2020-05-20 09:03:27 +0700

    #12.9 Facebook Login part Two

--- apollo.js
+++ apollo.js
@@ -1,5 +1,5 @@
 const options = {
-  uri: "http://localhost:4000/graphql",
+  uri: "http://11360246.ngrok.io",
 };
 
 export default options;
--- screens/Auth/Login.js
+++ screens/Auth/Login.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { TouchableWithoutFeedback, Keyboard } from "react-native";
 import AuthButton from "../../components/AuthButton";
 import AuthInput from "../../components/AuthInput";
 import useInput from "../../hooks/useInput";
 import { Alert } from "react-native";
+import { useMutation } from "react-apollo-hooks";
+import { LOG_IN } from "./AuthQueries";
 
 const View = styled.View`
   justify-content: center;
@@ -12,9 +14,15 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => {
-  const emailInput = useInput("");
-  const handleLogin = () => {
+export default ({ navigation, route }) => {
+  const emailInput = useInput(route.params ? route.params.email : "");
+  const [loading, setLoading] = useState(false);
+  const [requestSecretMutation] = useMutation(LOG_IN, {
+    variables: {
+      email: emailInput.value,
+    },
+  });
+  const handleLogin = async () => {
     const { value } = emailInput;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (value === "") {
@@ -23,6 +31,24 @@ export default () => {
       return Alert.alert("Please write an email");
     } else if (!emailRegex.test(value)) {
       return Alert.alert("That email is invalid");
+    }
+    try {
+      setLoading(true);
+      const {
+        data: { requestSecret },
+      } = await requestSecretMutation();
+      if (requestSecret) {
+        Alert.alert("Check your email");
+        navigation.navigate("Confirm", { email: value });
+      } else {
+        Alert.alert("Account not found");
+        navigation.navigate("Signup", { email: value });
+      }
+    } catch (e) {
+      console.log(e);
+      Alert.alert("Can't log in now");
+    } finally {
+      setLoading(false);
     }
   };
   return (
@@ -33,10 +59,10 @@ export default () => {
           placeholder="Email"
           keyboardType="email-address"
           returnKeyType="send"
-          onEndEditing={handleLogin}
+          onSubmitEditing={handleLogin}
           autoCorrect={false}
         />
-        <AuthButton onPress={handleLogin} text="Log In" />
+        <AuthButton loading={loading} onPress={handleLogin} text="Log In" />
       </View>
     </TouchableWithoutFeedback>
   );
